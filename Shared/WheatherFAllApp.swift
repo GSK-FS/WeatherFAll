809d8f41e031161e68f48957e08a7d3949e17920
GSK FS <GSK_FS@Live.com>
2021-12-06 01:41:59 +0500

    Initial Commit

---
+++ Shared/WheatherFAllApp.swift
@@ -0,0 +1,20 @@
+//
+//  WheatherFAllApp.swift
+//  Shared
+//
+//  Created by GSK on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct WheatherFAllApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
